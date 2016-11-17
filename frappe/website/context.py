7e86bd130d34330d2d2635b64e218fd4cd589a80
Omar Ali <oashour9@users.noreply.github.com>
2016-11-17 13:27:55 +0200

    webpage get_context exception handling

--- frappe/website/context.py
+++ frappe/website/context.py
@@ -6,6 +6,7 @@ import frappe
 
 from frappe.website.doctype.website_settings.website_settings import get_website_settings
 from frappe.website.router import get_page_context
+import frappe.utils
 
 def get_context(path, args=None):
 	if args and args.source:
@@ -43,10 +44,13 @@ def update_controller_context(context, controller):
 				context[prop] = getattr(module, prop)
 
 		if hasattr(module, "get_context"):
-			ret = module.get_context(context)
+			try:
+				ret = module.get_context(context)
+			except Exception:
+				frappe.errprint(frappe.utils.get_traceback())
 			if ret:
 				context.update(ret)
-
+				
 		if hasattr(module, "get_children"):
 			context.children = module.get_children(context)
 
